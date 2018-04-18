e588b19ee5c08abef529e0c03d930c2aa952d2c5
mauriycf <mauriycf@gmail.com>
2018-04-18 12:10:09 -0500

    Add files via upload

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+  "net/http"
+  "io"
+)
+
+func handler(w http.ResponseWriter, r *http.Request){
+  io.WriteString(w, "Hello World")
+}
+
+func main() {
+  http.HandleFunc("/", handler)
+  http.ListenAndServe(":8000", nil)
+}
